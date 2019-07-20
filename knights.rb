e814932c8c522942e1e12591011f4a164b1adf78
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-19 23:56:09 -0700

    Added ALL_VALID_MOVES

--- knights.rb
+++ knights.rb
@@ -1,8 +1,26 @@
 require_relative "polytreenode"
 class KnightPathFinder
+  ALL_VALID_MOVES = [
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+    [1, 2],
+    [2, 1]
+  ].freeze
 
   def initialize(starting_pos = [0,0])
-    @root_node = PolyTreeNode.new(starting_pos)
+    @root_node = build_move_tree(starting_pos)
+  end
+
+  def build_move_tree(starting_pos)
+    PolyTreeNode.new(starting_pos)
+  end
+
+  def valid_moves(pos)
+
   end
 
 end
