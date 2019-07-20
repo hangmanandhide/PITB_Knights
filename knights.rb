9f475294ee671e5eabacaf4109eaa0440cf026ad
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-20 00:23:19 -0700

    Setup valid_moves#KnightPathFinder to find array of all valid moves

--- knights.rb
+++ knights.rb
@@ -1,6 +1,6 @@
 require_relative "polytreenode"
 class KnightPathFinder
-  ALL_VALID_MOVES = [
+  DELTAS = [
     [2, -1],
     [1, -2],
     [-1, -2],
@@ -13,6 +13,7 @@ class KnightPathFinder
 
   def initialize(starting_pos = [0,0])
     @root_node = build_move_tree(starting_pos)
+    @considered_positions = starting_pos
   end
 
   def build_move_tree(starting_pos)
@@ -20,6 +21,25 @@ class KnightPathFinder
   end
 
   def valid_moves(pos)
+    all_valid_moves = []
+    board_size = 8
+
+    x, y = pos
+
+    DELTAS.each do |dx, dy|
+      valid_x = x + dx
+      valid_y = y + dy
+      valid_move = [valid_x, valid_y]
+
+      if valid_move.all? {|coord| coord.between?(0, board_size - 1)}
+        all_valid_moves << valid_move
+      end
+    end
+    
+    all_valid_moves
+  end
+
+  def new_move_positions(pos)
 
   end
 
